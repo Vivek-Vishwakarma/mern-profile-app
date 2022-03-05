1e043224f17c1dbf2c7c164a3dfc4c5b457d3a82
Vivek-Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2022-03-05 19:39:55 +0530

    added heroku scripts

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use("/api/profile", require("./routes/profileRoute"));
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
